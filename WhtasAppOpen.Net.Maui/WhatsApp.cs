50128a38e5cd8e9b09d8a52ef0e0b22c2b06b51e
MRMoha <MRMoha@DESKTOP-0CPCH1P>
2022-08-31 15:09:35 +0400

    Add project files.

---
+++ WhtasAppOpen.Net.Maui/WhatsApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhtasAppOpen.Net.Maui
+{
+    public class WhatsApp
+    {
+
+    
+
+
+        public static void OpenWithMessage(string phoneNumber, string msg = null)
+        {
+            try
+            {
+                var uri = "whatsapp://send?phone=" + phoneNumber;
+
+                if (!string.IsNullOrWhiteSpace(msg))
+                    uri += "&text=" + msg;
+
+                Launcher.Default.OpenAsync(new Uri(uri));
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+        public static void OpenNoMessage(string phoneNumber) {
+
+            var uri = "whatsapp://send?phone=" + phoneNumber;
+
+            Launcher.Default.OpenAsync(new Uri(uri));
+
+        }
+}
+}
